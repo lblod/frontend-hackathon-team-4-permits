2155912ed43aeaae212c01b514b89c88763a5579
Arne Kerremans <arne@inuits.eu>
2024-09-12 15:52:23 +0200

    draft search

---
+++ app/controllers/citizen/search-erfgoed.js
@@ -0,0 +1,44 @@
+import Controller from '@ember/controller';
+
+export default class SearchErfgoedController extends Controller {
+  get ids(){
+    const url = '/id-search?search=Bredabaan';
+
+    fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json(); 
+    })
+    .then(data => {
+      const ids = data.data.attributes.ids;
+      console.log('IDs:', ids);
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+    
+    this.getJsonWithId('98888')
+  }
+
+  getJsonWithId(id) {
+    return fetch(`https://inventaris.onroerenderfgoed.be/aanduidingsobjecten/${id}`, { 
+      headers: { 
+          "Accept": "application/json" 
+      } 
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+  })
+  .then(data => {
+      console.log('Data:', data);
+  })
+  .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+  });
+  }
+}
