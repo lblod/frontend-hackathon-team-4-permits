11421e198a20bc31c79140bd6b20bb21ab5062d1
Arne Kerremans <arne@inuits.eu>
2024-09-12 11:40:46 +0200

    add small flow for citizen

--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('citizen', function () {
+    this.route('index');
+    this.route('search-erfgoed');
+  });
+});
