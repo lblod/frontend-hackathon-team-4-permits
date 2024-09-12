50c7245e051ee5b3db42d295d72c2cb177f7da4d
Aad Versteden <madnificent@gmail.com>
2024-09-12 15:08:41 +0200

    More heritage link fixes :)

--- app/routes/citizen/index.js
+++ app/routes/citizen/index.js
@@ -3,8 +3,8 @@ import Route from '@ember/routing/route';
 export default class CitizenIndexRoute extends Route {
   model() {
     return [
-      { title: 'Gewad', date: '01/02/2024', decision: 'Goedgekeurd' },
-      { title: 'Belfort', date: '03/02/2024', decision: 'Geen' },
+      { title: 'Gewad', date: '01/02/2024', decision: 'Goedgekeurd', id: 2 },
+      { title: 'Belfort', date: '03/02/2024', decision: 'Geen', id: 3 },
     ];
   }
 }
--- app/routes/municipality/index.js
+++ app/routes/municipality/index.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class MunicipalityRoute extends Route {
   model() {
-    return [{ title: 'Gravensteen', date: '03/02/2024', decision: 'Geen' }];
+    return [{ title: 'Gravensteen', date: '03/02/2024', decision: 'Geen', id: 5 }];
   }
 }
