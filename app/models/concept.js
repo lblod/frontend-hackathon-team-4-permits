fbbdf9ed7350b40fdc4b1ee61d9039b2f598b41c
Aad Versteden <madnificent@gmail.com>
2024-09-13 14:21:14 +0200

    Fix spurious rendering of Ember Data content
    We had duplicate records.  This is Ember Data thinking it's smart where
    it's not.  We have to inform it not to make heuristically correct
    derivations when they don't hold.

--- app/models/concept.js
+++ app/models/concept.js
@@ -3,5 +3,5 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class ConceptModel extends Model {
   @attr('string') label;
   @attr('number') order;
-  @hasMany('concept') children;
+  @hasMany('concept', { inverse: null }) children;
 }
